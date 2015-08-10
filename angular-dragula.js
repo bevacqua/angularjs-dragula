4337cc4b522b037a3ba84d6ef4e573e94f12d458
Kevin OBrien <kevin.obrien@praece.com>
2015-08-09 23:51:16 -0700

    Moved directive to its own file with a register wrapper so we can pass in angular just once.

--- angular-dragula.js
+++ angular-dragula.js
@@ -2,35 +2,15 @@
 
 var dragula = require('dragula');
 var dragulaService = require('./service');
-var replicateEvents = require('./replicate-events');
+var dragulaDirective = require('./directive');
 
 function register (angular) {
   var app = angular.module('dragula', ['ng']);
 
-  app.factory('dragulaService', dragulaService);
-  app.directive('dragula', ['dragulaService', angularDragula]);
+  app.factory('dragulaService', dragulaService(angular));
+  app.directive('dragula', ['dragulaService', dragulaDirective(angular)]);
 
   return 'dragula';
-
-  function angularDragula (dragulaService) {
-    return {
-      restrict: 'A',
-      link: link
-    };
-
-    function link (scope, elem, attrs) {
-      var container = elem[0];
-      var name = scope.$eval(attrs.dragula);
-      var bag = dragulaService.find(scope, name);
-      if (bag) {
-        bag.drake.containers.push(container); return;
-      }
-      var drake = dragula({
-        containers: [container]
-      });
-      bag = dragulaService.add(scope, name, drake, angular);
-    }
-  }
 }
 
 module.exports = register;
